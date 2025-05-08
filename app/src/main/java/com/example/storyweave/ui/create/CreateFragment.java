23777245026c40dc753829035b1fdef422c8534a
Aleksander Galek <galuolek@gmail.com>
2025-05-08 11:07:15 +0200

    random code generator for nav create

--- app/src/main/java/com/example/storyweave/ui/create/CreateFragment.java
+++ app/src/main/java/com/example/storyweave/ui/create/CreateFragment.java
@@ -1,5 +1,10 @@
 package com.example.storyweave.ui.create;
 
+
+import android.app.Application;
+import android.content.ClipboardManager;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,11 +22,15 @@ import com.example.storyweave.GameActivity;
 import com.example.storyweave.R;
 import com.example.storyweave.databinding.FragmentCreateBinding;
 
+import java.util.Random;
+
 public class CreateFragment extends Fragment {
 
     private FragmentCreateBinding binding;
 
-    private Button btngame;
+
+    private TextView txtcopy;
+    private Button btngame, btncopy;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,6 +52,24 @@ public class CreateFragment extends Fragment {
         });
 
 
+        //random code*************************
+        txtcopy = root.findViewById(R.id.textcopy);
+
+        txtcopy.setText(generateSixDigitCode());
+
+        btncopy = root.findViewById(R.id.button_copy);
+
+        btncopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Code", txtcopy.getText());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(getActivity().getApplicationContext(), "String copied to Clipboard", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        //random code*************************
 
         return root;
     }
@@ -51,5 +79,24 @@ public class CreateFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public static String generateSixDigitCode(){
+
+        String code = "";
+        char[] uppercaseLetters = {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        for (int i = 0; i < 6; i++){
+            Random random = new Random();
+            if (Math.round(random.nextDouble()) == 0){
+                code += uppercaseLetters[random.nextInt(uppercaseLetters.length)];
+            } else {
+                code += numbers[random.nextInt(numbers.length)];
+            }
+        }
+        return code;
     }
 }
